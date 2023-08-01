3ee1b2cffb24c76275aef364708f7ac71bc38c23
kiwimiho <summer.shen.2007@gmail.com>
2023-08-01 15:43:11 -0700

    enemies done-ish

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     StateController controller;
-    public static int numWaypoints = 2;
-    public GameObject[] waypoints = new GameObject[numWaypoints];
+    // public static int numWaypoints = 2;
+    public GameObject[] waypoints = new GameObject[2];
     public float viewRange = 20;
 
     // Start is called before the first frame update
